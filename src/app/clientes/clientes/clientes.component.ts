bbd8bed88049b22817b1866da9112e7950623572
Piter Castro <castro.fcefyn@gmail.com>
2020-08-24 20:33:01 -0300

    Agregue el json en duro en la clase clientes.component.ts. En la vista clientes.component.html use un card de bootstrap. E inserte la tabla para ver los datos del json

--- src/app/clientes/clientes/clientes.component.ts
+++ src/app/clientes/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {
+      id: 1,
+      nombre: 'Roberto',
+      apellido: 'Abbondanzieri',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 2,
+      nombre: 'Roberto',
+      apellido: 'Ayala',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 3,
+      nombre: 'Juan Pablo',
+      apellido: 'Sorin',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 4,
+      nombre: 'Javier',
+      apellido: 'Zanetti',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 5,
+      nombre: 'Fernando',
+      apellido: 'Redondo',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 6,
+      nombre: 'Pedro',
+      apellido: 'Samuel',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 7,
+      nombre: 'Adrian',
+      apellido: 'Peralta',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 8,
+      nombre: 'Pedro',
+      apellido: 'Castro',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 9,
+      nombre: 'Gabriel',
+      apellido: 'Batistuta',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    },
+    {
+      id: 10,
+      nombre: 'Lionel',
+      apellido: 'Messi',
+      email: 'cp.castropedro@gmail.com',
+      createAt: '2020-08-24'
+    }
+  ]
 
   constructor() { }
 
