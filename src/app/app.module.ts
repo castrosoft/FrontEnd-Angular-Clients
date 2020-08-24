cddfcfb8b751efc3fb535010b4ff8ddc85362bfd
Piter Castro <castro.fcefyn@gmail.com>
2020-08-24 20:06:01 -0300

    Commiteo algunos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva/directiva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
