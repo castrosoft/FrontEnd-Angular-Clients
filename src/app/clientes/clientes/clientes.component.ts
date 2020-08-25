ce64f8ccf7c520d4c591518cfd1442ebd1350b70
Piter Castro <castro.fcefyn@gmail.com>
2020-08-24 22:11:35 -0300

    Pongo el JSON simulado en el nuevo archivo creado clientes.json.ts

--- src/app/clientes/clientes/clientes.component.ts
+++ src/app/clientes/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -8,82 +9,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {
-      id: 1,
-      nombre: 'Roberto',
-      apellido: 'Abbondanzieri',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 2,
-      nombre: 'Roberto',
-      apellido: 'Ayala',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 3,
-      nombre: 'Juan Pablo',
-      apellido: 'Sorin',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 4,
-      nombre: 'Javier',
-      apellido: 'Zanetti',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 5,
-      nombre: 'Fernando',
-      apellido: 'Redondo',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 6,
-      nombre: 'Pedro',
-      apellido: 'Samuel',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 7,
-      nombre: 'Adrian',
-      apellido: 'Peralta',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 8,
-      nombre: 'Pedro',
-      apellido: 'Castro',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 9,
-      nombre: 'Gabriel',
-      apellido: 'Batistuta',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    },
-    {
-      id: 10,
-      nombre: 'Lionel',
-      apellido: 'Messi',
-      email: 'cp.castropedro@gmail.com',
-      createAt: '2020-08-24'
-    }
-  ]
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
