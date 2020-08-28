24545c30f8b278c6ee65ea045765fe9d1a8c57d4
Piter Castro <castro.fcefyn@gmail.com>
2020-08-27 21:27:17 -0300

    Creo el metodo create en cliente.service.ts. Actualizo el metodo create en form.component.ts para que use los datos reales y luego rediriga a la pantalla listado de clientes

--- src/app/clientes/clientes/cliente.service.ts
+++ src/app/clientes/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
 
@@ -12,6 +12,8 @@ import { map } from 'rxjs/operators';
 export class ClienteService {
 
 private urlEndPoint: string = "http://localhost:8080/api/clientes";
+
+private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http: HttpClient) { }
 
@@ -41,8 +43,16 @@ private urlEndPoint: string = "http://localhost:8080/api/clientes";
       map( function(response) { return response as Cliente[]})
     );
     */
+  }
 
-
+  
+  /**
+   * 
+   * @param cliente :objeto Cliente JSON
+   * @returns: el objeto cliente que se creo en el API Rest
+   */
+  create(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
   }
 
 }
